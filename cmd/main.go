606ea4f50e090fefe0f4a9b4acaea12fed75e449
vlks <kosvov2001@yandex.ru>
2024-11-21 22:00:06 +0300

    first day, pgx migrations w/o tests

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
-package cmd
+package main
+
+import (
+	"context"
+	"github.com/vlks-dev/EffectiveMobileGoTest/config"
+	"github.com/vlks-dev/EffectiveMobileGoTest/migrations"
+	"github.com/vlks-dev/EffectiveMobileGoTest/shared/logger"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	slog := logger.NewSlog()
+	slog.Debug("config and logger initialized",
+		"configuration", cfg)
+	ctx := context.Background()
+	err := migrations.RunMigrations(ctx, slog, cfg)
+	if err != nil {
+		return
+	}
+}
